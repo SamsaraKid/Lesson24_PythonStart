caae5c5ddc4f571ff3eea380d0512bd38f12b22c
koany <koroliev@yandex.ru>
2023-05-28 14:34:51 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+# print('hello')
+# num = 0
+# print(num+1)
+# num = 12
+# print(num+1)
+#
+# sut = 24
+# hour = 60
+# minute = 60
+#
+# result = sut * minute * minute
+# print('секунд в сутках'+str(result))
+
+# v = 100
+# hour = 3600
+# meters = 1000
+# print(v, 'км/ч =', v * meters / hour, 'м/c')
+#
+# cat1 = 4
+# cats = 3
+# days = 7
+#
+# print(f'{cats} кота едят {cat1 * cats * days} пачек корма за {days} дней')
+
+# yourname = input('Как вас зовут?\n')
+# food = input('Что любите есть?\n')
+# movie = input('Ваш любимы фильм?\n')
+#
+# print('Привет %s. Давай купим %s и посмотрим %s' % (yourname, food, movie))
+
+# num1 = input('первое число\n')
+# num1 = int(num1)
+# num2 = input('второе число\n')
+# num2 = int(num2)
+# print(num1 * num2)
+
+nowYear = 2023
+year = int(input('Какой ваш год рождения?\n'))
+print('В этом году вам %d лет' % (nowYear - year))
+input()
